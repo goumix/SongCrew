dad146c129004c27c015e0f48e67e0b6fade44e6
goumix <nath.brault.pro@gmail.com>
2024-07-05 11:59:59 -0400

    add foundry to a hardhat project

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,5 +1,6 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
