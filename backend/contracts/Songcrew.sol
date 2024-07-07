13245ee2eb2434d78df04ed76d9ac64f4b9717f9
goumix <nath.brault.pro@gmail.com>
2024-07-07 17:27:05 -0400

    add structure Project

--- backend/contracts/Songcrew.sol
+++ backend/contracts/Songcrew.sol
@@ -1,28 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Songcrew {
-    uint public unlockTime;
-    address payable public owner;
+contract Songcrew is ERC1155 {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
 
-    event Withdrawal(uint amount, uint when);
+  struct Project {
+    string name;
+    string artist;
+    string genre;
+    string description;
+    uint numberOfCopies;
+  }
 
-    constructor() payable {
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
+  constructor() ERC1155("https://songcrew.com/api/project/{id}.json") {}
 }
