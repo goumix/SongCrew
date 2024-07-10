8322964d81994d0409ea24e491490e3d2c145740
goumix <nath.brault.pro@gmail.com>
2024-07-10 18:24:07 -0400

    style page project

--- frontend/src/app/project/[id]/page.tsx
+++ frontend/src/app/project/[id]/page.tsx
@@ -2,10 +2,14 @@
 import { publicClient } from '@/utils/client'
 import { useState, useEffect } from "react"
 import { contractAddress, contractAbi } from "@/constants";
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Slider } from '@/components/ui/slider';
 
 const Project = ({ params }: { params: { id: string }}) => {
 
   const [project, setProject] = useState<any>(null);
+  const [amount, setAmount] = useState<number>(1);
 
   const getProject = async () => {
     const set = await publicClient.readContract({
@@ -22,9 +26,25 @@ const Project = ({ params }: { params: { id: string }}) => {
   }, []);
 
   return (
-    <div className='w-full min-h-screen p-10'>
+    <div className='w-full min-h-screen flex flex-col p-10'>
       <h1><strong>Project</strong></h1>
-      <p>Name : {project?.title}</p>
+      <div className='w-full flex flex-row'>
+        <div className='w-1/2 mt-6'></div>
+        <div className='w-1/2 h-screen flex flex-col justify-center gap-4 pb-40'>
+          <div className="flex flex-row items-center gap-6">
+            <h1><strong>{project?.title}</strong></h1>
+            <Badge>{project?.genre}</Badge>
+          </div>
+          <p><strong>Artist :</strong> {project?.artist}</p>
+          <p><strong>Description :</strong> {project?.description}</p>
+          <p><strong>Number of NFTs remaining :</strong> {Number(project?.numberOfCopies)}</p>
+          <p><strong>Number of NFT you buy :</strong> {amount}</p>
+          <div className='w-2/3 flex flex-row justify-around gap-4'>
+            <Slider defaultValue={[1]} max={Number(project?.numberOfCopies)} min={1} step={1} className="py-2" onValueChange={(e) => setAmount(e)}/>
+            <Button>Buy</Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
