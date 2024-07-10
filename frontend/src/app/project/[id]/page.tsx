abf6736b86dc23ba821e5e7725bd5ce035a77fec
goumix <nath.brault.pro@gmail.com>
2024-07-10 17:59:09 -0400

    add request for project

--- frontend/src/app/project/[id]/page.tsx
+++ frontend/src/app/project/[id]/page.tsx
@@ -1,8 +1,30 @@
+'use client'
+import { publicClient } from '@/utils/client'
+import { useState, useEffect } from "react"
+import { contractAddress, contractAbi } from "@/constants";
+
 const Project = ({ params }: { params: { id: string }}) => {
+
+  const [project, setProject] = useState<any>(null);
+
+  const getProject = async () => {
+    const set = await publicClient.readContract({
+      abi: contractAbi,
+      address: contractAddress,
+      functionName: 'getOneProject',
+      args: [params.id]
+    });
+    setProject(set);
+  }
+
+  useEffect(() => {
+    getProject();
+  }, []);
+
   return (
     <div className='w-full min-h-screen p-10'>
       <h1><strong>Project</strong></h1>
-      <p>Id : {params.id}</p>
+      <p>Name : {project?.title}</p>
     </div>
   )
 }
