b06f842933c2a16a2f830461069a876cb4e71e01
goumix <nath.brault.pro@gmail.com>
2024-07-12 17:53:56 -0400

    update hardhat test

--- backend/test/Songcrew.test.ts
+++ backend/test/Songcrew.test.ts
@@ -4,57 +4,87 @@ import {
 } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
+import { parseAbiItem } from "viem";
 import { getAddress, parseGwei } from "viem";
 
 describe("Songcrew", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  async function deploy() {
+    const [address1, address2] = await hre.viem.getWalletClients();
 
-    const lockedAmount = parseGwei("1");
-    const unlockTime = BigInt((await time.latest()) + ONE_YEAR_IN_SECS);
-
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await hre.viem.getWalletClients();
-
-    const lock = await hre.viem.deployContract("Songcrew");
+    const songcrew = await hre.viem.deployContract("Songcrew");
 
     const publicClient = await hre.viem.getPublicClient();
 
     return {
-      lock,
-      unlockTime,
-      lockedAmount,
-      owner,
-      otherAccount,
+      songcrew,
+      address1,
+      address2,
       publicClient,
     };
   }
 
-  describe("Deployment", function () {
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+  describe("Create Project", function () {
+    it("Should not create a project with 0 number of copies", async function () {
+      const { songcrew, address1 } = await loadFixture(deploy);
 
-      expect(await lock.read.owner()).to.equal(
-        getAddress(owner.account.address)
-      );
+      await expect(songcrew.write.createProject(["Artist", "ID123", "Title", "Genre", "Description", 1n, 0n,], { account: address1.account.address })).to.be.rejectedWith("ERC1155: number of copies must be greater than 0");
+    });
+
+    it("Should not create a project with 100 number of copies", async function () {
+      const { songcrew, address1 } = await loadFixture(deploy);
+
+      await expect(songcrew.write.createProject(["Artist", "ID123", "Title", "Genre", "Description", 1n, 100n,], { account: address1.account.address })).to.be.rejectedWith("ERC1155: number of copies must be less than or equal to 70");
+    });
+
+    it("Should not create a project with 0 price project", async function () {
+      const { songcrew, address1 } = await loadFixture(deploy);
+
+      await expect(songcrew.write.createProject(["Artist", "ID123", "Title", "Genre", "Description", 0n, 10n,], { account: address1.account.address })).to.be.rejectedWith("ERC1155: price of the project must be greater than 0");
+    });
+
+    it("Should not create a project with 0 price project", async function () {
+      const { songcrew, address1 } = await loadFixture(deploy);
+
+      await expect(songcrew.write.createProject(["Artist", "ID123", "Title", "Genre", "Description", 20n, 10n,], { account: address1.account.address })).to.be.rejectedWith("ERC1155: price of the project must be less than or equal to 16");
+    });
+
+    it("Should create a project", async function () {
+      const { songcrew, address1 } = await loadFixture(deploy);
+
+      await expect(songcrew.write.createProject(["Artist", "ID123", "Title", "Genre", "Description", 10n, 10n,], { account: address1.account.address })).to.be.fulfilled;
+      songcrew.read.getOneProject([0n]).then((project) => {
+        expect(project.artist).to.equal("Artist");
+        expect(project.id).to.equal("ID123");
+        expect(project.title).to.equal("Title");
+        expect(project.genre).to.equal("Genre");
+        expect(project.description).to.equal("Description");
+        expect(project.priceProject).to.equal(10n);
+        expect(project.numberOfCopies).to.equal(10n);
+        expect(project.priceNft).to.equal(1n);
+      });
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  describe("Buy NFT", function () {
+    it("Should not buy a NFT does not exist", async function () {
+      const { songcrew, address1, address2 } = await loadFixture(deploy);
 
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
+      await songcrew.write.createProject(["Artist", "ID123", "Title", "Genre", "Description", 10n, 10n,], { account: address1.account.address });
+      await expect(songcrew.write.buyNft([1n, 1n], { account: address2.account.address })).to.be.rejectedWith("ERC1155: project does not exist");
+    });
 
-        await expect(lock.write.withdraw()).to.be.fulfilled;
-      });
+    it("Should not buy a NFT with 0 amount", async function () {
+      const { songcrew, address1, address2 } = await loadFixture(deploy);
+
+      await songcrew.write.createProject(["Artist", "ID123", "Title", "Genre", "Description", 10n, 10n,], { account: address1.account.address });
+      await expect(songcrew.write.buyNft([0n, 0n], { account: address2.account.address })).to.be.rejectedWith("ERC1155: amount must be greater than 0");
+    });
+
+    it("Should not buy a NFT with too much amount", async function () {
+      const { songcrew, address1, address2 } = await loadFixture(deploy);
+
+      await songcrew.write.createProject(["Artist", "ID123", "Title", "Genre", "Description", 10n, 10n,], { account: address1.account.address });
+      await expect(songcrew.write.buyNft([0n, 11n], { account: address2.account.address })).to.be.rejectedWith("ERC1155: amount must be less than or equal to the number of copies");
     });
   });
 });
