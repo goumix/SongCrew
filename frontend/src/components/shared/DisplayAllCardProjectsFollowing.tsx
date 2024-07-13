860c4cd0d99ddb1e090515653081232075cf2e45
goumix <nath.brault.pro@gmail.com>
2024-07-13 13:24:09 -0400

    display following projects

--- frontend/src/components/shared/DisplayAllCardProjectsFollowing.tsx
+++ frontend/src/components/shared/DisplayAllCardProjectsFollowing.tsx
@@ -4,33 +4,41 @@ import { contractAddress, contractAbi } from "@/constants";
 import { publicClient } from "@/utils/client";
 import { parseAbiItem } from "viem";
 import CardProjectFollowing from "../ui/CardProjectFollowing";
+import { useAccount } from "wagmi";
 
 
 const DisplayAllCardProjectsFollowing = () => {
   const [projects, setProjects] = useState<any[]>([]);
+  const [balanceSender, setBalanceSender] = useState<any[]>([]);
+
+  const { address } = useAccount();
 
   const getProjects = async () => {
-    const depositEvents = await publicClient.getLogs({
+    const getBalanceSender = await publicClient.readContract({
+      abi: contractAbi,
       address: contractAddress,
-      event: parseAbiItem('event ProjectCreatedNumber(uint256 projectId)'),
-      fromBlock: 0n,
-      toBlock: 'latest'
+      functionName: 'getBalanceOfAllProjectsForTheSender',
+      account: address
     });
+    setBalanceSender(getBalanceSender);
+    console.log(getBalanceSender);
 
-    const eventsWithDetails = await Promise.all(depositEvents.map(async (event) => {
-      const data = await publicClient.readContract({
-        abi: contractAbi,
-        address: contractAddress,
-        functionName: 'getOneProject',
-        args: [event.args.projectId]
-      });
-      return {
-        ...event,
-        details: data
-      };
+    const projectSender = await Promise.all(getBalanceSender.map(async (project: any, index: number) => {
+      if (Number(project) !== 0) {
+        const data = await publicClient.readContract({
+          abi: contractAbi,
+          address: contractAddress,
+          functionName: 'getOneProject',
+          args: [index]
+        });
+        return {
+          ...project,
+          details: data
+        };
+      }
     }));
 
-    setProjects(eventsWithDetails);
+    setProjects(projectSender.filter((project: any) => project !== undefined));
   };
 
   useEffect(() => {
@@ -39,9 +47,10 @@ const DisplayAllCardProjectsFollowing = () => {
 
   return (
     <div className="flex flex-wrap justify-start gap-4 mt-6">
+      {projects.length === 0 && <p>You are not following any project</p>}
       {projects.map((project, index) => (
         <div key={index} className="w-[32%]">
-          <CardProjectFollowing project={project} />
+          <CardProjectFollowing project={project} amount={Number(balanceSender[index])} />
         </div>
       ))}
     </div>
--- frontend/src/components/ui/CardProjectFollowing.tsx
+++ frontend/src/components/ui/CardProjectFollowing.tsx
@@ -1,17 +1,17 @@
 import { Badge } from "../ui/badge"
 import Card from "./Card"
 
-const CardProjectFollowing = ({ project }: any) => {
+const CardProjectFollowing = ({ project, amount }: any) => {
   return (
     <Card>
-      <div>
-        <div className="flex flex-row items-center gap-6">
+      <div className="h-40 flex flex-col justify-around">
+        <div className="flex flex-row items-center gap-4">
           <h1><strong>{project.details.title}</strong></h1>
           <Badge>{project.details.genre}</Badge>
         </div>
         <p>Artist : {project.details.artist}</p>
         <p>Description : {project.details.description}</p>
-        <p>number of NFTs you own : {Number(project.details.numberOfCopies)}</p>
+        <p>number of NFTs you own : {amount}</p>
       </div>
     </Card>
   )
