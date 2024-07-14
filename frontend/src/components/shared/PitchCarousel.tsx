91ce53d7aa8531bc44a2c72d59d722899f21bf76
goumix <nath.brault.pro@gmail.com>
2024-07-14 05:42:20 -0400

    update carousel

--- frontend/src/components/shared/PitchCarousel.tsx
+++ frontend/src/components/shared/PitchCarousel.tsx
@@ -5,25 +5,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import Image from "next/image"
 
 const Pitch = [
   {
-    title: "Pitch 1",
-    description: "SONGCREW permet au public de devenir producteur de ses artistes favoris.",
+    title: "1. Songcrew",
+    description1: "Une dApp qui permet au public de devenir producteur de ses artistes favoris.",
+    description2:  "Une plateforme d'achat de droits d'auteur musicaux avant la création et la sortie d'un projet musical.",
+    image: "/songcrew-pitch1-removebg-preview.png",
   },
   {
-    title: "Pitch 2",
-    description: "Description 2",
+    title: "2. NFT",
+    description1: "L'artiste met en vente ses droits d'auteur sous forme de NFT pour financer son projet.",
+    description2: "L'utilisateur peut acheter ces NFT et percevoir les droits d'auteur directement dans son wallet.",
+    image: "/songcrew-nft.png",
   },
   {
-    title: "Pitch 3",
-    description: "Description 3",
+    title: "3. Revenus",
+    description1: "Les revenus générés par le projet sont versés directement sur la dApp qui se charge de redistribuer les revenus aux détenteurs de NFT.",
+    image: "/songcrew-eth-logo-removebg-preview.png",
   },
-  {
-    title: "Pitch 4",
-    description: "Description 4",
-  },
-
 ]
 
 export function PitchCarousel() {
@@ -32,9 +33,13 @@ export function PitchCarousel() {
       <CarouselContent>
         {Pitch.map((pitch, index) => (
           <CarouselItem key={index}>
-            <div className="h-96 w-full bg-white rounded-lg">
-              <h1 className="text-black">{pitch.title}</h1>
-              <p>{pitch.description}</p>
+            <div className={`h-96 w-full flex ${index % 2 === 0 ? "flex-row": "flex-row-reverse"} justify-between gap-10 bg-slate-700 rounded-lg p-12`}>
+              <div className="flex flex-col justify-around">
+                <h1><strong>{pitch.title}</strong></h1>
+                <p>{pitch.description1}</p>
+                <p>{pitch.description2}</p>
+              </div>
+              <Image src={pitch.image} alt={pitch.title} width={500} height={500} />
             </div>
           </CarouselItem>
         ))}
